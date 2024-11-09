2381013eff3910d791d8ab53eba70b87ee579fee
Kien Nguyen <knguyen2@macalester.edu>
2024-11-08 23:48:29 -0600

    part 2 step 1

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -3,17 +3,45 @@ module TimeTravel exposing (addTimeTravel)
 import Playground exposing (..)
 import Set
 
+controlBarHeight = 64
+
 addTimeTravel rawGame = 
-    { view = viewWithTimeTravel rawGame
-    , initialState = initialStateWithTimeTravel rawGame
+    { initialState = initialStateWithTimeTravel rawGame
     , updateState =updateWithTimeTravel rawGame
+    , view = viewWithTimeTravel rawGame
     }
 
-initialStateWithTimeTravel rawGame = rawGame.initialState
+initialStateWithTimeTravel rawGame = 
+    { rawModel = rawGame.initialState
+    , paused = False
+    }
 
 viewWithTimeTravel rawGame computer model = 
-    rawGame.view computer model
+    let
+        helpMessage =
+            if model.paused then
+            "Press R to resume"
+            else
+            "Press T to time travel"
+    in
+        (rawGame.view computer model.rawModel) ++
+            [ words white helpMessage
+                |> move 0 (computer.screen.top - controlBarHeight / 2)
+            ]
 
 updateWithTimeTravel rawGame computer model = 
-    rawGame.updateState computer model
+    if keyPressed "T" computer then
+        {model | paused = True}
+    else if keyPressed "R" computer then
+        {model | paused = False}
+    else if model.paused then
+        model
+    else
+        { model | rawModel = rawGame.updateState computer model.rawModel }
+
+keyPressed keyName computer =
+  [ String.toLower keyName
+  , String.toUpper keyName
+  ]
+    |> List.any (\key -> Set.member key computer.keyboard.keys)
 
